401e1eec929d9e1be58dba5e40212e551fb60c48
Luis Felipe <luisfelipekai@gmail.com>
2021-01-29 13:15:39 -0300

    Pré-Live 3.

--- DevEvents.API/Controllers/CategoriasController.cs
+++ DevEvents.API/Controllers/CategoriasController.cs
@@ -1,19 +1,24 @@
-﻿using DevEvents.API.Entidades;
+﻿using DevEvents.API.Persistencia;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevEvents.API.Controllers
 {
     [Route("api/categorias")]
     public class CategoriasController : ControllerBase
     {
+        private readonly DevEventsDbContext _dbContext;
+        public CategoriasController(DevEventsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         [HttpGet]
         public IActionResult ObterTodas()
         {
-            return Ok();
+            var categorias = _dbContext.Categorias.ToList();
+
+            return Ok(categorias);
         }
     }
 }
--- DevEvents.API/Controllers/UsuariosController.cs
+++ DevEvents.API/Controllers/UsuariosController.cs
@@ -1,4 +1,5 @@
 ﻿using DevEvents.API.Entidades;
+using DevEvents.API.Persistencia;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevEvents.API.Controllers
@@ -6,9 +7,18 @@ namespace DevEvents.API.Controllers
     [Route("api/usuarios")]
     public class UsuariosController : ControllerBase
     {
+        private readonly DevEventsDbContext _dbContext;
+        public UsuariosController(DevEventsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         [HttpPost]
         public IActionResult Cadastrar([FromBody] Usuario usuario)
         {
+            _dbContext.Usuarios.Add(usuario);
+            _dbContext.SaveChanges();
+
             return Ok();
         }
     }
